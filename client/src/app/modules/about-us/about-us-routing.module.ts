94a89e78d76509fe2a75db89a713242b45e4de1d
Towa Quimbayo <towacurtis@hotmail.com>
2024-06-07 15:58:23 -0700

    formatted modules

--- client/src/app/modules/about-us/about-us-routing.module.ts
+++ client/src/app/modules/about-us/about-us-routing.module.ts
@@ -5,6 +5,6 @@ const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AboutUsRoutingModule { }
+export class AboutUsRoutingModule {}
--- client/src/app/modules/about-us/about-us.module.ts
+++ client/src/app/modules/about-us/about-us.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AboutUsRoutingModule } from './about-us-routing.module';
-
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    AboutUsRoutingModule
-  ]
+  imports: [CommonModule, AboutUsRoutingModule],
 })
-export class AboutUsModule { }
+export class AboutUsModule {}
