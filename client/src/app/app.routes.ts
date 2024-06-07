28b43476468acf65e382d15ec1f5214d2e3a516c
Towa Quimbayo <towacurtis@hotmail.com>
2024-06-07 00:38:53 -0700

    ud home route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [{ path: 'home', component: HomeComponent }];
+export const routes: Routes = [{ path: '', component: HomeComponent }];
