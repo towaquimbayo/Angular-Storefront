17681f2b8508d8ac38dfe1b9af48650a4b0df955
Towa Quimbayo <towacurtis@hotmail.com>
2024-06-07 13:52:38 -0700

    added pagination

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../componnets/product/product.component';
@@ -7,7 +8,7 @@ import { ProductComponent } from '../componnets/product/product.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -15,14 +16,27 @@ export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
   products: Product[] = [];
+  totalRecords: number = 0;
+  rows: number = 5;
 
   onProductOutput(product: Product) {
     console.log(product);
   }
 
-  ngOnInit() {
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
     this.productsService
-      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
-      .subscribe((products: Products) => this.products = products.items);
+      .getProducts('http://localhost:3000/clothes', { page, perPage })
+      .subscribe((products: Products) => {
+        this.products = products.items;
+        this.totalRecords = products.total;
+      });
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
   }
 }
