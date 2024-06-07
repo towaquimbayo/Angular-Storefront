2c5f921b56b9e5f6fc33af97650e00997da2a728
Towa Quimbayo <towacurtis@hotmail.com>
2024-06-07 00:46:45 -0700

    Created a generic GET API service with types

--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Options } from '../../types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  /**
+   * Generic GET request
+   * @param url The URL of the request
+   * @param options The options of the request 
+   * @returns The response of the request
+   */
+  get<T>(url: string, options: Options): Observable<T> {
+    return this.httpClient.get<T>(url, options) as Observable<T>;
+  }
 }
