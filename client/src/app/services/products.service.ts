d5f8e8341dde7a344187b0415017684929245b9f
Towa Quimbayo <towacurtis@hotmail.com>
2024-06-07 00:56:52 -0700

    Created a Get Request handler for Product API Service

--- client/src/app/services/products.service.ts
+++ client/src/app/services/products.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+import { PaginationParams, Products } from '../../types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  constructor(private apiService: ApiService) {}
 
-  constructor() { }
+  getProducts = (
+    url: string,
+    params: PaginationParams
+  ): Observable<Products> => {
+    return this.apiService.get<any>(url, { params, responseType: 'json' });
+  };
 }
