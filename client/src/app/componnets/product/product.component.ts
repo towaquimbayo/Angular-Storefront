729f40d65168ac40abe25072114156bcc3d69827
Towa Quimbayo <towacurtis@hotmail.com>
2024-06-07 13:36:42 -0700

    displayed products from DB

--- client/src/app/componnets/product/product.component.ts
+++ client/src/app/componnets/product/product.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'primeng/rating';
+import { Product } from '../../../types';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [RatingModule, FormsModule],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.scss'
+  styleUrl: './product.component.scss',
 })
 export class ProductComponent {
+  // ! always assumes that the value is not null or undefined
+  @Input() product!: Product;
+  @Output() productOutput: EventEmitter<Product> = new EventEmitter<Product>();
 
+  ngOnInit() {
+    this.productOutput.emit(this.product);
+  }
 }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ProductsService } from '../services/products.service';
-import { Products } from '../../types';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../componnets/product/product.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
+  products: Product[] = [];
+
+  onProductOutput(product: Product) {
+    console.log(product);
+  }
+
   ngOnInit() {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
-      .subscribe((products: Products) => console.log(products));
+      .subscribe((products: Products) => this.products = products.items);
   }
 }
