fdd742c29ce2f0b731c1bf378784ba8f5f00702a
Towa Quimbayo <towacurtis@hotmail.com>
2024-06-07 16:06:07 -0700

    added About Us and Faq in Modules routing

--- client/src/app/modules/about-us/about-us.module.ts
+++ client/src/app/modules/about-us/about-us.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutUsRoutingModule } from './about-us-routing.module';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FaqComponent } from './faq/faq.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AboutUsComponent, FaqComponent],
   imports: [CommonModule, AboutUsRoutingModule],
 })
 export class AboutUsModule {}
