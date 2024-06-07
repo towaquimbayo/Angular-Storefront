591e78847365259e4235d70e5b0fce632a76c820
Towa Quimbayo <towacurtis@hotmail.com>
2024-06-07 16:06:29 -0700

    added Faq to about us routes

--- client/src/app/modules/about-us/about-us-routing.module.ts
+++ client/src/app/modules/about-us/about-us-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { FaqComponent } from './faq/faq.component';
 
-const routes: Routes = [{
-  path: '',
-  component: AboutUsComponent,
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'faq',
+    component: FaqComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
